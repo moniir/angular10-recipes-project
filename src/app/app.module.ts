2ca106a35dddabb573469368ad7098e4613f7ad1
monir <monir198@gmail.com>
2020-10-24 11:38:06 +0600

    added project all components and re-structured components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
+import {ReceipesComponent} from './receipes/receipes.component';
+import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
+import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { from } from 'rxjs';
+import { ReceipeItemComponent } from './receipes/receipe-list/receipe-item/receipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    ReceipesComponent,
+    HeaderComponent,
+    ReceipeListComponent,
+    ReceipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    ReceipeItemComponent
   ],
   imports: [
     BrowserModule
