948df5b14963fb99ce14edae527fe0bf4d11c0cf
monir <monir198@gmail.com>
2020-10-24 13:44:50 +0600

    added recipe list and recipe detail

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Receipe } from '../receipe';
 
 @Component({
   selector: 'app-receipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receipe-list.component.css']
 })
 export class ReceipeListComponent implements OnInit {
+
+  receipes: Receipe[] = [
+    new Receipe('A Test Receipe','Test Recipes Description','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/9/5/1/WU0306H_perfect-french-fries_s4x3.jpg.rend.hgtvcom.826.620.suffix/1589465976850.jpeg')
+  ];
 
   constructor() { }
 
