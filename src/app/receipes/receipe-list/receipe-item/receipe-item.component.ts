a969659d83c8bf2b71942a9baaed168a08c10ccc
Monir Hossain <monir198@gmail.com>
2020-11-21 13:37:29 +0600

    splited recipe list with recipe item component
    splited recipe list with recipe item component

--- src/app/receipes/receipe-list/receipe-item/receipe-item.component.ts
+++ src/app/receipes/receipe-list/receipe-item/receipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Receipe } from '../../receipe';
 
 @Component({
   selector: 'app-receipe-item',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceipeItemComponent implements OnInit {
 
+  @Input() recipe: Receipe
   constructor() { }
 
   ngOnInit(): void {
